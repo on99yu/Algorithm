8a40cc2438e8d26eddc874b26b37a2f208c5116a
on99yu <dgjjanggu@naver.com>
2022-03-03 12:41:14 +0900

    java

---
+++ Java/Datatype.java
@@ -0,0 +1,7 @@
+public class Datatype {
+    public static void main(String[] aStrings){
+        System.out.println(6);
+        System.out.println("six");
+        System.out.println("1111".length());
+    }
+}
